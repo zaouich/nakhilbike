944baae79d8522251397995be4aee1079b64efe8
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-07-19 19:23:28 +0100

    Update ShowSale.jsx

--- cltside/src/components/ShowSale.jsx
+++ cltside/src/components/ShowSale.jsx
@@ -45,7 +45,9 @@ const resizeImage = async (imageUrl) => {
 const ShowSale = () => {
   const { id } = useParams();
   const [sale, setSale] = useState({});
+  const [imgs, setImgs] = useState(false);
   const [resizedImages, setResizedImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
     axios
@@ -57,12 +59,14 @@ const ShowSale = () => {
         // Resize images when sale data is fetched
         const resizedImagePromises = sale.buyer_card.map((el) => {
           const imageUrl = `http://localhost:3000/static/imgs/card/${el}`;
+          setImgs(true);
           return resizeImage(imageUrl);
         });
 
         // Wait for all resized image promises to resolve
         Promise.all(resizedImagePromises).then((resizedImageURIs) => {
           setResizedImages(resizedImageURIs);
+          setIsLoading(false); // Set the loading state to false when all images are loaded
         });
       })
       .catch((err) => alert(err.response.data.message));
@@ -113,24 +117,28 @@ const ShowSale = () => {
         <div className="card mb-3">
           <h5 className="card-header">صور المشتري</h5>
           <div className="card-body">
-            <div className="row row-cols-3 g-5">
-              {resizedImages.map((smallImageUri, index) => {
-                const imageUrl = `http://localhost:3000/static/imgs/card/${sale.buyer_card[index]}`;
+            {isLoading ? ( // Render a loading indicator while the images are loading
+              <div>جاري تحميل الصور ... </div>
+            ) : (
+              <div className="row row-cols-3 g-5">
+                {resizedImages.map((smallImageUri, index) => {
+                  const imageUrl = `http://localhost:3000/static/imgs/card/${sale.buyer_card[index]}`;
 
-                return (
-                  <div key={index}>
-                    <ModalImage
-                      small={smallImageUri}
-                      large={imageUrl}
-                      showRotate
-                      alt={`صورة ${index + 1}`}
-                      width={200} // Set the desired width here
-                      height={200} // Set the desired height here
-                    />
-                  </div>
-                );
-              })}
-            </div>
+                  return (
+                    <div key={index}>
+                      <ModalImage
+                        small={smallImageUri}
+                        large={imageUrl}
+                        showRotate
+                        alt={`صورة ${index + 1}`}
+                        width={200} // Set the desired width here
+                        height={200} // Set the desired height here
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </div>
       </div>
