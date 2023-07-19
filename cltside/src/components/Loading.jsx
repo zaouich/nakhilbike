4e625752366c989b592bc58dc17b648f380f1fb6
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-07-19 15:19:40 +0100

    Update Loading.jsx

--- cltside/src/components/Loading.jsx
+++ cltside/src/components/Loading.jsx
@@ -1,6 +1,8 @@
-const Loading = ()=>{
-    return <>
-        <h1>loading......</h1>
+const Loading = () => {
+  return (
+    <>
+      <h1>loading..</h1>
     </>
-}
-export default Loading
+  );
+};
+export default Loading;
