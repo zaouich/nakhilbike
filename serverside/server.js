53a2dc61a821aaa9e27cf0f5f5e81f03ab3486b4
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-07-19 15:02:13 +0100

    Update server.js

--- serverside/server.js
+++ serverside/server.js
@@ -2,8 +2,8 @@ const app = require("./app");
 const dotenv = require("dotenv").config({ path: "./config.env" });
 const mongoose = require("mongoose");
 const DB = process.env.DB;
-app.listen(3001, () => {
-  console.log("listning to the port 3001.....");
+app.listen(3000, () => {
+  console.log("listning to the port 3000.....");
   mongoose.connect(DB).then(() => {
     console.log("db connected");
   });
